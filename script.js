867ef76b4b23b2f5bd72d5766a09701821ed313a
BardiaLatifi <Bardialatifi70@gmail.com>
2025-01-11 13:45:34 +0330

    no swiper library

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// import Swiper bundle with all modules installed
-import Swiper from './swiper/bundle';
-
-// import styles bundle
-import './swiper/css/bundle.main.css';
-
-// Initialize Swiper
 const swiper = new Swiper('.swiper', {
-  loop: true,
+
+  // If we need pagination
   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    el: '.swiper-pagination',
   },
+
+  // Navigation arrows
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
   },
 });
